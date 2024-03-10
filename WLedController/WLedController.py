a8ac280c6d550f646d368c9f3afaa5fe83d5a3a4
Tjorven Burdorf (TG-650) <Tjorven.Burdorf@bmw.de>
2024-03-10 23:07:17 +0100

    continued change to json-api

--- WLedController/WLedController.py
+++ WLedController/WLedController.py
@@ -57,7 +57,8 @@ class WLedController:
                 raise ArgumentNotValidException(arguments)
 
         except:
-            self._logging(f"major error occured with argument {arguments}")
+            if self.log:
+                self._logging(f"major error occured with argument {arguments}")
 
             raise Exception(f"'{arguments}' resulted in an Error.")
 
@@ -82,80 +83,32 @@ class WLedController:
 
         self.send_arguments(self._build_data(("ps", preset)))
 
-    def set_color(self, color: tuple[int, int, int]):
+    def set_primary_color(self, color: tuple[int, int, int]):
         if min(color) < 0 or max(color) > 255:
             raise ValueOutOfBoundsException(color, (0, 0, 0), (255, 255, 255))
 
-        self.send_arguments(self._build_data(("col", '['+str(list(color))+']')))
+        self.send_arguments(self._build_data(("col", '[' + str(list(color)) + ']')))
 
     def set_effect(self, effect: int):
         if effect < 0 or effect > 101:
             raise ValueOutOfBoundsException(effect, stop=101)
 
-        self.send_arguments(f"FX={effect}")
+        self.send_arguments(self._build_data(("fx", effect)))
 
     def set_effect_speed(self, speed: int):
         if speed < 0 or speed > 255:
             raise ValueOutOfBoundsException(speed)
 
-        self.send_arguments(f"SX={speed}")
+        self.send_arguments(self._build_data(("sx", speed)))
 
     def set_effect_intensity(self, intensity: int):
         if intensity < 0 or intensity > 255:
             raise ValueOutOfBoundsException(intensity)
 
-        self.send_arguments(f"IX={intensity}")
+        self.send_arguments(self._build_data(("ix", intensity)))
 
-    def set_palette(self, palette: int):
-        if palette < 0 or palette > 255:
-            raise ValueOutOfBoundsException(palette)
+    def freeze(self):
+        self.send_arguments(self._build_data(("frz", 'true')))
 
-        self.send_arguments(f"FP={palette}")
-
-    def stop_timer(self):
-        self.deactivate()
-        sleep(.1)
-        self.activate()
-
-    def set_timer(self, target_brightness: int = 0, timer: int = None):
-        if timer and (min(target_brightness, timer) < 0 or max(target_brightness, timer) > 255):
-            raise ValueOutOfBoundsException(timer)
-
-        if not timer:
-            self.send_arguments("ND", f"NT={target_brightness}")
-        else:
-            self.send_arguments(f"NL={timer}", f"NT={target_brightness}")
-
-    def hour_marker(self, timeout: int):
-        last_marker = datetime.now().hour
-
-        while True:
-            now = datetime.now()
-            not_off = "<ac>0</ac>" not in self.get_status()
-            unmarked_new_hour = now.minute == 0 and now.hour != last_marker
-
-            if not_off and unmarked_new_hour:
-                timestamp = str(now)[:-7]
-                print(f"{timestamp}: hour marked", flush=True)
-
-                last_marker = now.hour
-
-                iteration_range = now.hour % 12 - 1
-                if iteration_range < 0:
-                    iteration_range = 12
-
-                self.send_arguments("PS=255")
-                self.set_preset(1)
-                sleep(1)
-                for _ in range(iteration_range):
-                    self.send_arguments("A=16")
-                    sleep(1)
-                    self.set_preset(1)
-                    sleep(1)
-
-                self.set_preset(255)
-
-            sleep(timeout)
-
-
-print(WLedController("192.168.178.42").set_color((25, 25, 25)))
+    def unfreeze(self):
+        self.send_arguments(self._build_data(("frz", 'false')))
