fcc03028c670e6d6ae1d8db4461dcf48a28ece19
tj0vtj0v <87753458+tj0vtj0v@users.noreply.github.com>
2024-03-11 07:51:10 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='WLedController',
+    packages=['WLedController'],
+    version='1.0',
+    license='MIT',
+    description='simple implementation of the json-api from WLed https://kno.wled.ge/interfaces/json-api/',
+    author='Tjorven Burdorf',
+    author_email='BurdorfTjorven@gmail.com',
+    url='https://github.com/tj0vtj0v/WLedController',
+    download_url='',
+    keywords=['WLed', 'IoT', 'Connector', 'API'],
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
