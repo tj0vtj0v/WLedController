abffc6f0fa5757b9ce69a0ba13f86a8985dbc5bf
tj0vtj0v <burdorftjorven@gmail.com>
2024-03-14 19:05:13 +0100

    hotfix exception error

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='WLedController',
     packages=['WLedController'],
-    version='0.2',
+    version='0.2.1',
     license='MIT',
     description='simple implementation of the json-api from WLed https://kno.wled.ge/interfaces/json-api/',
     author='Tjorven Burdorf',
     author_email='BurdorfTjorven@gmail.com',
     url='https://github.com/tj0vtj0v/WLedController',
-    download_url='https://github.com/tj0vtj0v/WLedController/archive/refs/tags/Beta.tar.gz',
+    download_url='',
     keywords=['WLed', 'IoT', 'Connector', 'API'],
     install_requires=['requests'],
     classifiers=[
